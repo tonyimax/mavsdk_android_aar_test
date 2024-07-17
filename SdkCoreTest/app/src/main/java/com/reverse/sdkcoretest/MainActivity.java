700673e832604348696da56c9a69c312ba5d86fe
WK_QT_DEV <wk13342864161@outlook.com>
2024-07-17 11:12:09 +0800

    增加返航

--- SdkCoreTest/app/src/main/java/com/reverse/sdkcoretest/MainActivity.java
+++ SdkCoreTest/app/src/main/java/com/reverse/sdkcoretest/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         btnRtl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                c.Rtl();
+                c.Rtl();//返航
             }
         });
     }
